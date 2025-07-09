fe2fbbb6fc82ba11266d95f509289334ff05bd9c
aristotlechain <131588160+aristotlechain@users.noreply.github.com>
2025-07-08 20:43:43 -0400

    Create main_nginxOnK8s.tf

---
+++ main_nginxOnK8s.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  # update this path to point to your kubeconfig
+  config_path = "/Users/aristotle/.kube/config" // run the other main.tf present in this repo, then run az aks get-credentials --resource-group aks-lab --name aks-lab-cluster, this will give you the path which you need to copy here for the config
+}
+
+resource "kubernetes_namespace" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx-deployment"
+    namespace = kubernetes_namespace.nginx.metadata[0].name
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:1.14.2"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "nginx-service"
+    namespace = kubernetes_namespace.nginx.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
